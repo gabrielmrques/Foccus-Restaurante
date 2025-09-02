c5e1741953b7e23c0e7d03b6d2541e0005c0d0ec
Gabriel <gabrimarquesniq@gmail.com.br>
2025-09-02 11:28:03 -0300

    feat add shadcn initial setup components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ const ProductPage = () => {
   return (
     <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">products page</h1>
-      <Button>FSW 7.0</Button>
+      <Button>CLIQUE AQUI</Button>
       <Input placeholder="Bora fechar esse projeto!" />
     </div>
   );
